89f3e9d84c9b6617d2c6bffc26ed7b87450ce6e9
AlgoWebBot <shimulzahan636@gmail.com>
2023-07-31 00:57:18 +0600

    first module js

---
+++ Module 1/jan.js
@@ -0,0 +1,85 @@
+// function cheek(n) {
+//     if (n % 2 === 0) {
+//         factorial(n);
+//     }
+//     else {
+//         console.log("Odd number")
+//     }
+// }
+// function factorial(number) {
+//     var sum = 1;
+//     for (var i = number; i > 0; i--) {
+//         sum += i * sum;
+//         console.log(sum);
+//         console.log(17 % 5);
+//         var name = ABUL;
+//     }
+// }
+// cheek(12);
+
+
+// a) Find the index of ‘Banana’ and replace ‘Banana’ with ‘Mango’.
+// b) Remove ‘Orange’ and add ‘Watermelon’.
+// var fruits = ['Apple', 'Banana', 'Orange'];
+// const index = fruits.indexOf("Banana");
+// fruits[1] = "Mango";
+// console.log(fruits);
+// console.log(fruits.pop());
+// fruits.push('Watermelon');
+// console.log(fruits);
+
+// You and your friends Tom, Jane, Peter and John got their final exam results. Your total
+// score is 85, Tom’s total score is 66, Jane’s total score is 95, Peter’s total score is 56
+// and John’s total score is 40. The grading chart is
+// 80 or above A grade
+// 60 or above B grade
+// 50 or above C grade
+// 40 or above D grade
+// 39 or less => F grade
+
+// Write a program to find your and your friends’ grades using if-else.
+// var number = 85;
+// var number = 66;
+// var number = 95;
+
+// if (number >= 80) {
+//     console.log('A');
+// } else if (number >= 60) {
+//     console.log('B');
+// } else if (number >= 50) {
+//     console.log('C');
+// } else if (number >= 40) {
+//     console.log('D')
+// }else {
+//     console.log('F');
+// }
+
+
+// 1. You are given three numbers 13, 79, and 45. Write a program that will print the
+// largest number using if-else.
+// var number = 193;
+// var number1 = 279;
+// var number2 = 205;
+// if (number > number1) {
+//     if (number > number2) {
+//         console.log(number + " Biggest Number");
+//     }
+//     else {
+//         console.log(number2 + " Biggest Number");
+//     }
+// } else if (number2 > number1) {
+//     console.log(number2 + " Biggest Number")
+// } else {
+//     console.log(number1 + " Biggest Number");
+// }
+
+// 2. You are given a triangle with the sides 9, 8, 9. Write a program to check whether a
+// triangle is Isosceles or not using if-else.
+// var length = 9;
+// var height = 8;
+// var wide = 9;
+// if (height == wide || height == length || length == wide) {
+//     console.log("Isometric");
+// } else {
+//     console.log("Not Isometric");
+// }
